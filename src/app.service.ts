44b5787c6af7af4cb73b96bec3d9a917cffbcc59
Johannes / EinJojo <schlotmax507@gmail.com>
2023-07-31 09:20:33 +0200

    stuff

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,39 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  sendVerificationEmail() {
+    //
+  }
+
+  sendWelcomeEmail() {
+    // On Purchase
+  }
+
+  sendUpdateEmail() {
+    //
+  }
+
+  sendNewsletter() {
+    //
+  }
+
+  sendPasswordReset() {
+    //
+  }
+
+  sendPasswordChanged() {
+    //
+  }
+
+  sendPasswordChangedNotification() {
+    //
+  }
+
+  sendOrderConfirmation() {
+    //
+  }
+
+  sendOrderShipped() {
+    //
   }
 }
