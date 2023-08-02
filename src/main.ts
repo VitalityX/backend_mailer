9647a10635ddb6f5ccee526bea94b5b1fc92845b
Johannes / EinJojo <schlotmax507@gmail.com>
2023-08-02 16:41:14 +0200

    kafkajs - use default partition

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ExceptionFilter } from './error.interceptor';
+import { Partitioners } from 'kafkajs';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.KAFKA,
+      
       options: {
+        producer: {
+          createPartitioner: Partitioners.DefaultPartitioner
+        },
         client: {
           clientId: 'mailer',
           brokers: (process.env.KAFKA_BROKERS || '').split(','),  
