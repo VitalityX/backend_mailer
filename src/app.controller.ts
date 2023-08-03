2b839ac345fa6aa78b850a8d50de4c9dee510084
Johannes / EinJojo <schlotmax507@gmail.com>
2023-08-04 01:07:19 +0200

    mailer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,17 @@
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EventPattern, RpcException } from '@nestjs/microservices';
+import { MailerPayloads } from './dto/MailerPayloads.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @EventPattern("sendVerificationEmail")
-  sendVerificationEmail(payload: any) {
+  sendVerificationEmail(payload: MailerPayloads.EmailVerificationPayload) {
     if (!payload) {
       throw new RpcException("No payload provided")
     }
-    this.appService.sendNewsletter()
+    this.appService.sendVerificationEmail(payload)
   }
 }
